fb3015d18f951b905aaea805fafdcb0a878e34e9
Sahil Sapehia <officialsahilsapehia03@gmail.com>
2025-08-22 16:28:37 +0530

    create eureka server where services will be registered

---
+++ service-registery-service/src/test/java/com/sapehia/service_registery_service/ServiceRegisteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapehia.service_registery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
